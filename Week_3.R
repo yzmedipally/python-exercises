7cd8da9bb6bcb0c2b0048aaca4bfbb8590f8908b
Hidde Bleeker <hiddebleeker2@gmail.com>
2017-11-20 22:54:56 +0100

    added genbank format file

--- Week_3.R
+++ Week_3.R
@@ -3,4 +3,38 @@
 # Created by: hidde
 # Created on: 17/11/17
 
+### DESeq2 R practical
 
+library(DESeq2)
+source("http://www.bioinformatics.nl/courses/BIF-30806/DEseq2Exercise.R")
+internode_data=read.table(
+  "http://www.bioinformatics.nl/courses/BIF-30806/maize_e3.table",
+  row.names=1, header=TRUE, sep ="\t")
+
+new_internode_data = internode_data[mx > 10,]
+dim(new_internode_data)
+
+count_data = new_internode_data[,1:6]
+condition = factor(c("first","first","first","fourth","fourth","fourth"))
+col_data = DataFrame(condition)
+
+dds = DESeqDataSetFromMatrix(count_data, col_data, ~condition)
+dds = estimateSizeFactors(dds)
+sizeFactors(dds)
+
+norm_versus_non_norm(dds, 1, 2, left = 2, right = 8)
+rld = rlog(dds)
+plot(density(assay(dds)[,1]), main="counts")
+plot(density(assay(rld)[,1]), main="log counts")
+dists = dist(t(assay(rld)))
+plot(hclust(dists))
+
+dds = estimateDispersions(dds)
+plotDispEsts(dds)
+dds = nbinomWaldTest(dds)
+res = results(dds)
+
+res$padj = ifelse(is.na(res$padj), 1, res$padj)
+res$annotation = new_internode_data[,7]
+write.table(res, col.names=NA, row.names=T, file ="internodes.tsv", sep ="\t")
+plotMA(res, main="MA plot",ylim=c(-8,8),alpha=0.01)
