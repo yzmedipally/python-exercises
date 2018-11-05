cc7bc4d216c1e60938cc0bc1d35ed18838e41fa1
Hidde Bleeker <hiddebleeker2@gmail.com>
2018-11-05 18:14:54 +0100

    P2 Improved structure and clarity

--- P2/parsing.py
+++ P2/parsing.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Parse a GenBank formatted input file, output FASTA file and tab-delimited statistics file
+"""Parse a GenBank formatted input file, output FASTA and tab-delim. stats files
 
 Author: Hidde Bleeker
 931202071020
@@ -9,30 +9,29 @@ usage: {} input.gb output_filename
 
 # Imports
 from sys import argv
+from operator import itemgetter
 
 
 def parse_gb_input(gb_filename):
-    """Parse a GenBank formatted file and return dictionary with information per record
+    """Parse a GenBank formatted file and return dictionary with info per record
 
     :param gb_filename: str, input filename
-    :return: generator, with list entries as [accession_number, (organism_name, sequence)]
+    :return: generator, with elements as accession_nr, organism_name, sequence
     """
-    accession, org_name, seq = [], [], []
+    accession, org_name, seq = [], "", []
     reading_seq = False
     with open(gb_filename) as gb_ip:
         for line in gb_ip:
-            # print(line.strip(), line.startswith("//"))
             if line.startswith('//'):
-                # print("startswith //")
                 reading_seq = False
-                yield accession, (org_name, "".join(seq))
-                accession, org_name, seq = [], [], []
+                yield accession, org_name, "".join(seq)
+                accession, org_name, seq = [], "", []
             elif reading_seq:
                 for partial_seq in line.strip().upper().split()[1:]:
                     seq.append(partial_seq)
                 continue
             elif line.startswith('ACCESSION'):
-                # Collect (only) primary accession number
+                # Collect (only!) primary accession number
                 accession = line.strip().split()[1]
             elif line.startswith('  ORGANISM'):
                 org_name = " ".join(line.strip().split()[1:])
@@ -50,50 +49,54 @@ def calc_gc_cont(dna_seq):
     return sum(count) / len(count) * 100
 
 
-def write_sorted_fasta(seq_dict, output_fn):
+def write_sorted_fasta(seqs_sort, output_fn):
     """Print fasta file from given sequence dictionary
 
-    :param seq_dict: dict, with GB accession numbers as keys and (organism, seqeuences,
-    GC_percentage) as values
-    :param output_fn: Name of the output file to print the sequences to, without extension
-    :return: None, it is a function that writes a file output
+    :param seqs_sort: list, with tuples as (GB accession numbers,
+    {org_name, seq, GC_percentage}); sorted on order of printing
+    :param output_fn: Name of the output file to print the sequences to, without
+     extension
+    :return: None, it is a function that writes an output file
     """
-    seqs_sorted = sorted(seq_dict.items(), key=lambda v: v[1][2], reverse=True)
     with open(output_fn + '.fasta', 'w') as out_f:
-        for entry in seqs_sorted:
-            out_f.write("".join(['>', str(entry[0]), ' ', str(entry[1][0]), '\n']))
-            out_f.write(entry[1][1] + '\n')
+        for entry in seqs_sort:
+            out_f.write(">{acc:s} {org_name:s}\n{seq:s}\n".format(**entry))
 
 
-def write_sorted_stats(seq_dict, output_fn):
+def write_sorted_stats(seqs_sort, output_fn):
     """Print statistics file from given sequence dictionary
 
-    :param seq_dict: dict, with GB accession numbers as keys and (organism, seqeuences,
-    GC_percentage) as values
-    :param output_fn: Name of the output file to print the sequences to, without extension
-    :return:
+    :param seqs_sort: list, with tuples as (GB accession numbers,
+    {org_name, seq, GC_percentage}); sorted on order of printing
+    :param output_fn: Name of the output file to print the sequences to, without
+     extension
+    :return: None, it is a function that writes an output file
     """
-    seqs_sorted = sorted(seq_dict.items(), key=lambda v: v[1][2], reverse=True)
     with open(output_fn + '.csv', 'w') as out_f:
-        for entry in seqs_sorted:
-            out_f.write("{0:s}\t{1:s}\t{2:.2f}\t{3:d}\n"
-                        .format(entry[0], entry[1][0], entry[1][2], len(entry[1][1])))
+        for entry in seqs_sort:
+            out_f.write("{acc:s}\t{org_name:s}\t{GC_content:.2f}\t{length:d}\n"
+                        .format(**entry))
 
 
 if __name__ == '__main__':
     # Check for right amount of input arguments
     if len(argv) != 3:
-        print("Not the right amount of command line arguments given...\nQuitting.")
+        print("Not the right amount of command line arguments given...\n"
+              "Quitting.")
         print(__doc__.format(argv[0]))
         exit(1)
     # Check if second input argument has the correct file extension
     elif not argv[1].lower().endswith('.gb'):
-        print("File in incorrect input format given (should be .gb)...\nQuitting.")
+        print("File in incorrect input format given (should be .gb)...\n"
+              "Quitting.")
         print(__doc__.format(argv[0]))
         exit(1)
     else:
-        # Create dictionary from parsed GB sequences and write FASTA/stats to files.
-        sequences = {acc: (name, seq, calc_gc_cont(seq)) for acc, (name, seq) in
-                     parse_gb_input(argv[1])}
-        write_sorted_fasta(sequences, argv[2])
-        write_sorted_stats(sequences, argv[2])
+        # Create dictionary from parsed GB sequences and write FASTA/stats to
+        # files.
+        sequences = [{'acc': acc, 'org_name': name, 'seq': seq, 'GC_content': calc_gc_cont(seq), 'length': len(seq)} for acc, name, seq in parse_gb_input(argv[1])]
+
+        sorted_sequences = sorted(sequences, key=itemgetter('GC_content'),
+                                  reverse=True)
+        write_sorted_fasta(sorted_sequences, argv[2])
+        write_sorted_stats(sorted_sequences, argv[2])
