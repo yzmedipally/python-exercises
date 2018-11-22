e0458983842b90b562309a8e544f1defccb92268
Hidde Bleeker <hiddebleeker2@gmail.com>
2018-11-22 23:21:18 +0100

    Test exam; Finished(?)

--- P6/bleek020_test_exam.py
+++ P6/bleek020_test_exam.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Script to
+"""Script to compare assembled genome to reference, with help of LASTZ aligner
 
 Author: Hidde Bleeker (931202071020)
 """
@@ -10,7 +10,6 @@ import subprocess as sbp
 from sys import argv
 from operator import itemgetter
 from os.path import exists
-
 
 
 def parse_arguments():
@@ -93,7 +92,7 @@ def run_lastz(reference_ip, query_ip, output_format='general',
     :param str query_ip: Filename of the query sequence (FASTA format)
     :param str output_format: format type for the lastz utility
     :param str output_fn: Filename to write output to.
-    :return:
+    :return: None, command calling function
     """
     if exists(output_fn):
         print("Output file already exists; lastz not run.")
@@ -101,7 +100,7 @@ def run_lastz(reference_ip, query_ip, output_format='general',
     cmd = "lastz {} {} --format={} --output={}"\
         .format(reference_ip, query_ip, output_format, output_fn)
 
-    print("Running LASTZ, command used:\n{}".format(cmd))
+    # print("Running LASTZ, command used:\n{}".format(cmd))
     try:
         sbp.check_call(cmd, shell=True)
     except sbp.CalledProcessError as err:
@@ -109,8 +108,50 @@ def run_lastz(reference_ip, query_ip, output_format='general',
     # implicit return
 
 
-def parse_lastz_output(list_of_lines):
+def parse_lastz_output(list_of_lines, header=True):
+    """Parse the output file of LASTZ for alignments
 
+    :param generator list_of_lines: LASTZ file, as list of lines to parse
+    :param bool header: Indicating if input file has a header
+    :return: generator of alignments, with coordinates (0-based, open-ended)
+    parsed from lastz file
+    :rtype generator
+    """
+    for line in list_of_lines:
+        if header:
+            header = False
+            continue
+        if line.strip():
+            fields = line.strip().split('\t')
+            yield int(fields[4]), int(fields[5])
+
+
+def find_uncovered(seq_len, covered_coords):
+    """Find the positions not present in sequence and return those as indices
+
+    :param int seq_len: Length of the sequence
+    :param iterator covered_coords: iterator of pairs of ints; these are the
+    ranges which are present (or "covered").
+    :return: list of pairs of the ranges that are uncovered by the input
+    coordinates; returned as 0-based, half-open indices.
+    :rtype generator of pairs of ints
+    """
+    uncovered = [True, ] * seq_len
+    for coords in covered_coords:
+        for i in range(coords[0], coords[1]):
+            uncovered[i] = False
+
+    curr = []
+    for i, not_covered in enumerate(uncovered):
+        if not_covered:
+            if not curr:
+                curr = [i, i + 1]
+            else:
+                curr[-1] += 1
+        else:
+            if curr:
+                yield tuple(curr)
+            curr = []
 
 
 if __name__ == '__main__':
@@ -120,31 +161,47 @@ if __name__ == '__main__':
     # # In this case every args[key] has thus to be replaced with argv[1] or
     # # argv[2].
 
+    # Parse arguments and read in (parse) FASTA files
     args = parse_arguments()
     # print(type(args), args)
     with open(args['query']) as inp_f:
-        ASSEMBLY_DICT = {_id: (_seq, len(_seq)) for _id, _seq in
-                         parse_fasta(inp_f)}
+        QUERY_DICT = {_id: (_seq, len(_seq)) for _id, _seq in
+                      parse_fasta(inp_f)}
     with open(args['reference']) as inp_f:
         REFERENCE_DICT = {_id: (_seq, len(_seq)) for _id, _seq in
                           parse_fasta(inp_f)}
 
-    print(find_size_and_n50(ASSEMBLY_DICT))
-    print(find_size_and_n50(REFERENCE_DICT))
+    # Run LASTZ for sequence alignment
+    run_lastz(args['reference'], args['query'], output_fn="outlastz.txt")
 
-    run_lastz(args['reference'], args['query'])
+    # Find unaligned positions from LASTZ output file.
+    with open("outlastz.txt") as lastz_f:
+        ALIGNED_POSITIONS = list(parse_lastz_output(lastz_f))
+    UNALIGNED_POSITION = find_uncovered(next(iter(REFERENCE_DICT.values()))[1],
+                                        ALIGNED_POSITIONS)
 
+    # Print the genome stats and the uncovered regions
+    print("{}: TOTAL={}; N50 SIZE={}; N50 INDEX={}"
+          .format(args['query'], *find_size_and_n50(QUERY_DICT)))
+    print("{}: TOTAL={}; N50 SIZE={}; N50 INDEX={}"
+          .format(args['reference'], *find_size_and_n50(REFERENCE_DICT)))
+    print("Uncovered regions:")
+    GENOME_SEQ = next(iter(REFERENCE_DICT.values()))[0]
+    count_total = [0, 0]
+    for e in UNALIGNED_POSITION:
+        print("{}:{} {:s}".format(e[0], e[1], GENOME_SEQ[e[0]:e[1]][:30]))
+        count_total[0] += 1
+        count_total[1] += (e[1] - e[0])
+    print("Number of uncovered regions: {}\nNumber of uncovered bases: {}"
+          .format(*count_total))
 
-
-
-
-    # for _val in ASSEMBLY_DICT.values():
+    # Testing purposes...
+    # for _val in QUERY_DICT.values():
     #     print(_val[1])
     # print("\n\n")
     # for _val in REFERENCE_DICT.values():
     #     print(_val[1])
-
-    # first = next(iter(ASSEMBLY_DICT))
-    # print(first, ASSEMBLY_DICT[first][1])
+    # first = next(iter(QUERY_DICT))
+    # print(first, QUERY_DICT[first][1])
     # first = next(iter(REFERENCE_DICT))
     # print(first, REFERENCE_DICT[first][1])
