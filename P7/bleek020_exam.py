b1feed46b8f8dded56bc07f84767d61cb873e5dc
Hidde Bleeker <hiddebleeker2@gmail.com>
2018-11-23 08:49:38 +0100

    Exam part 1.

---
+++ P7/bleek020_exam.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+"""Script to
+
+Author: Hidde Bleeker (931202071020)
+"""
+
+# Imports
+import argparse
+import subprocess as sbp
+from sys import argv
+from os.path import exists
+
+
+def parse_arguments():
+    """Create an argument parser and parse arguments from argv
+
+    :return: Parsed arguments; as {argument_name: argument_value}
+    :rtype: dict of {str: str}
+    """
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('assembly', type=lambda inp: inp
+                        if any(inp.lower().endswith(tail) for tail in
+                               ['.fa', '.fasta'])
+                        else parser.error("Wrong input file extension."),
+                        help="Input genome assembly FASTA file, with .fasta or"
+                             " .fa file extension.")
+    if len(argv) <= 1:
+        parser.print_help()
+        exit(1)
+    else:
+        return vars(parser.parse_args())
+
+
+def parse_fasta(lines):
+    """Parse a FASTA file. Sequences are stored by ID in a dictionary.
+
+    :param lines: iterator of lines (e.g. in a FASTA file).
+    :returns: generator of dictionary of {label:dna_seq}
+    """
+    label, seq = "", []
+    for line in lines:
+        if not line.strip() or line.startswith(';'):
+            continue
+        if line.startswith('>'):
+            if label:
+                yield label, "".join([subseq for subseq in seq])
+            label = line.strip()[1:]
+            seq = []
+        else:
+            seq.append(line.strip())
+    yield label, "".join(seq)  # seq is already an iterator
+
+
+def run_augustus(in_fn, genemodel='complete', gff3='on', out_fn='aug.gff',
+                 species='aspergillus_nidulans'):
+    """Run augustus with parameters supplied if output file does not exist
+
+    :param str in_fn: Input (query) filename
+    :param str genemodel: Augustus genemodel parameter, default is 'complete'
+    :param str gff3: Augustus gff3 parameter, default is 'on'
+    :param str out_fn: Output filename, default is 'aug.gff'
+    :param str species: Augustus species parameter, default is
+    'aspergillus_nidulans'
+    :return: None, calls command line utility
+    """
+    if exists(out_fn):
+        print("## Output file already exists; augustus was not run.")
+        return
+    cmd = "augustus --genemodel {} --gff3 {} --outfile {} --species {} {}"\
+        .format(genemodel, gff3, out_fn, species, in_fn)
+
+    print("## Running augustus, command used:\n## {}".format(cmd))
+    try:
+        sbp.check_call(cmd, shell=True)
+    except sbp.CalledProcessError as err:
+        print("## Augustus failed to run. Error:\n## {}\n## Quitting."
+              .format(err))
+    # implicit return
+
+
+if __name__ == '__main__':
+    # # In the case that argparse is not allowed:
+    # if not len(argv) == 3:
+    #     print("usage: python3 {} reference.fa query.fa".format(argv[0]))
+    # # In this case every ARGS[key] has thus to be replaced with argv[1] or
+    # # argv[2].
+
+    # Read command line arguments into dictionary:
+    ARGS = parse_arguments()
+    # print(type(ARGS), ARGS)
+
+    run_augustus(ARGS['assembly'])
+
+    with open(ARGS['assembly']) as inp_f:
+        ASSEMBLY = {_id: (_seq, len(_seq)) for _id, _seq in
+                    parse_fasta(inp_f)}
